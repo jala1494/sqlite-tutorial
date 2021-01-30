ef6055d5684fb42ed18fc2c163a7e25f5b4b0dd6
jala1494 <jusung1633@gmail.com>
2021-01-30 19:43:56 +0900

    function is working

--- lib/main.dart
+++ lib/main.dart
@@ -6,19 +6,67 @@
 
 import 'dart:async';
 
+import 'package:example/database_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp((MaterialApp(
+    home: HomePage(),
+  )));
 }
 
-class MyApp extends StatelessWidget {
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title: Text('sqlite tutorial'),
-    ));
+      appBar: AppBar(
+        title: Text('sqlite tutorial'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FlatButton(
+              onPressed: () async {
+                int i = await DatabaseHelper.instance
+                    .insert({DatabaseHelper.columnName: 'Saheb'});
+                print('the inserted id is $i');
+              },
+              child: Text('insert'),
+              color: Colors.grey,
+            ),
+            FlatButton(
+              onPressed: () async {
+                List<Map<String, dynamic>> queryRows =
+                    await DatabaseHelper.instance.queryAll();
+                print(queryRows);
+              },
+              child: Text('query'),
+              color: Colors.green,
+            ),
+            FlatButton(
+              onPressed: () async {
+                int updatedId = await DatabaseHelper.instance.update({
+                  DatabaseHelper.columnId: 12,
+                  DatabaseHelper.columnName: 'Mark'
+                });
+                print(updatedId);
+              },
+              child: Text('update'),
+              color: Colors.blue,
+            ),
+            FlatButton(
+              onPressed: () async {
+                int rowsEffected = await DatabaseHelper.instance.delete();
+                print(rowsEffected);
+              },
+              child: Text('delete'),
+              color: Colors.red,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
